54d2911a2b9c43829475e0ee3712f6cff8d7323f
sahuno <ekwame001@gmail.com>
2023-08-09 10:33:28 -0400

    added raw prom data

---
+++ README.md
@@ -0,0 +1,66 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# methylONT
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of methylONT is to porvide utility functions to differential
+methylation analysis for lonfg read sequencing
+
+## Installation
+
+You can install the development version of methylONT from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("sahuno/methylONT")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(methylONT)
+#> Warning: replacing previous import 'IRanges::shift' by 'data.table::shift' when
+#> loading 'methylONT'
+#> Warning: replacing previous import 'Biostrings::tail' by 'utils::tail' when
+#> loading 'methylONT'
+#> Warning: replacing previous import 'IRanges::stack' by 'utils::stack' when
+#> loading 'methylONT'
+#> Warning: replacing previous import 'IRanges::relist' by 'utils::relist' when
+#> loading 'methylONT'
+#> Warning: replacing previous import 'Biostrings::head' by 'utils::head' when
+#> loading 'methylONT'
+## basic example code
+```
+
+What is special about using `README.Rmd` instead of just `README.md`?
+You can include R chunks like so:
+
+``` r
+summary(cars)
+#>      speed           dist       
+#>  Min.   : 4.0   Min.   :  2.00  
+#>  1st Qu.:12.0   1st Qu.: 26.00  
+#>  Median :15.0   Median : 36.00  
+#>  Mean   :15.4   Mean   : 42.98  
+#>  3rd Qu.:19.0   3rd Qu.: 56.00  
+#>  Max.   :25.0   Max.   :120.00
+```
+
+You’ll still need to render `README.Rmd` regularly, to keep `README.md`
+up-to-date. `devtools::build_readme()` is handy for this. You could also
+use GitHub Actions to re-render `README.Rmd` every time you push. An
+example workflow can be found here:
+<https://github.com/r-lib/actions/tree/v1/examples>.
+
+You can also embed plots, for example:
+
+<img src="man/figures/README-pressure-1.png" width="100%" />
+
+In that case, don’t forget to commit and push the resulting figure
+files, so they display on GitHub and CRAN.
--- data-raw/gene_promoters_encode1kb.R
+++ data-raw/gene_promoters_encode1kb.R
@@ -9,9 +9,12 @@ library(BSgenome.Hsapiens.UCSC.hg38)
 #load("/juno/work/greenbaum/users/ahunos/apps/methylONT/data/gene_promoters_encode1kb.rda")
 #gene_promoters_encode1kb
 
+#specify paths to data
+path_2_hg38TSS <- "/juno/work/greenbaum/users/ahunos/methyl_SPECTRUM/data/ref/ENCFF493CCB.bed.gz"
 
-dt <- fread("/juno/work/greenbaum/users/ahunos/methyl_SPECTRUM/data/ref/ENCFF493CCB.bed.gz")
-head(dt)
+
+dt <- fread(path_2_mm10TSS)
+#head(dt)
 names(dt) <- c("seqnames", "start", "end", "ensgene.version", "score", "strand")
 #dim(dt)
 dt[, ensgene:=(gsub("\\..*","",ensgene.version))]
@@ -64,16 +67,14 @@ usethis::use_data(gene_promoters_encode1kb_proteinCoding, overwrite = TRUE)
 
 
 ##############################################################################
-####################
+##############################################################################
 #create data sets 
-
 ff <- "/work/greenbaum/projects/ont_pipeline/projects/SPECTRUM_MTHY/results/methylation/megalodon/Spectrum-OV-044_N/per_read_modified_base_calls.txt"
 dt1 <- data.table::fread(ff, nrows = 100)
 cmd = glue::glue("tail -n 100 {ff}")
 dt2 <- data.table::fread(cmd = cmd, header=TRUE)
 per_mod_base_DT <- data.table::rbindlist(list(dt1, dt2))
 usethis::use_data(per_mod_base_DT, overwrite = TRUE)
-
 
 ##testing how the reads are organized
 # load("/juno/work/greenbaum/users/ahunos/apps/methylONT/data/per_mod_base_DT.rda")
@@ -82,4 +83,59 @@ dt2 <- dt[!grep("_",chrm),][, `:=`(exp_mod_log_prob = exp(mod_log_prob), exp_can
 summary(dt2$sum_mod_can_prob)
 summary(dt2$sum_Exp_mod_can_prob)                                    
 split(dt2, dt2$mod_base)
+###################################################################################
 
+
+
+
+###################################################################################
+#############Mouse data promoters and enhancers ####################
+###################################################################################
+path_2_mm10TSS <- "/juno/work/greenbaum/users/ahunos/methyl_SPECTRUM/data/ref/encode/ATAC-seq/mm10/ENCFF498BEJ.bed.gz"
+
+dt <- fread(path_2_mm10TSS)
+names(dt) <- c("seqnames", "start", "end", "ensgene.version", "score", "strand")
+dt[, ensgene:=(gsub("\\..*","",ensgene.version))]
+#dt[, .N, by = ensgene.version][N>1,] #sanity check, is any transcript annotated more than once?
+
+grcm38_dt <- grcm38 %>% as.data.table()
+grcm38_dt <- grcm38_dt[, .(ensgene, entrez, symbol, biotype, description)]
+dt <- dt[grcm38_dt, on = "ensgene", nomatch = NULL]
+
+dt <- dt[, .(seqnames, start, end, strand, ensgene.version, symbol, biotype, key = paste0(symbol,"_" ,ensgene.version, strand))] #save only needed parts 
+setkey(dt, key)
+
+dt <- unique(dt, by = "key") #remove duplicate promoters
+# dt[, fullDup := .N > 1, by = key(dt)] #check for full duplicates in the data
+# dt_keyed =  dt_keyed[fullDup == ,]
+
+#add to utils scripts
+message("adding transcription start site")
+dt[,`:=`(transcription_start_site = fcase(strand == "+", start, strand == "-", end))] #set transcription start site
+
+dt[,.N, by = biotype] #check biotype
+
+gr_tss_encode_mm10 <- GenomicRanges::makeGRangesFromDataFrame(dt, keep.extra.columns = TRUE, starts.in.df.are.0based = TRUE)
+gene_promoters_encode1kb_mm10 <- GenomicRanges::promoters(gr_tss_encode_mm10, upstream = 1000, downstream = 1000)
+
+#get num_GCs in gene promoters 
+library(BSgenome.Mmusculus.UCSC.mm10)
+genome <- BSgenome.Mmusculus.UCSC.mm10
+#head(seqlengths(genome), 23)
+gene_promoters_encode1kb_mm10_Seq <- getSeq(genome, gene_promoters_encode1kb_mm10)
+gene_promoters_encode1kb_mm10_numGCs <- Biostrings::vcountPattern("GC", gene_promoters_encode1kb_mm10_Seq)
+gene_promoters_encode1kb_mm10_numCpGs <- Biostrings::vcountPattern("CG", gene_promoters_encode1kb_mm10_Seq)
+
+mcols(gene_promoters_encode1kb_mm10)$numGCs <- gene_promoters_encode1kb_mm10_numGCs
+mcols(gene_promoters_encode1kb_mm10)$numCGs <- gene_promoters_encode1kb_mm10_numCpGs
+
+gene_promoters_encode1kb_mm10_proteinCoding <- gene_promoters_encode1kb_mm10[gene_promoters_encode1kb_mm10$biotype == "protein_coding"]
+
+saveRDS(gene_promoters_encode1kb_mm10_proteinCoding, file = "inst/extdata/gene_promoters_encode1kb_mm10_proteinCoding.rds")
+saveRDS(gene_promoters_encode1kb_mm10, file = "inst/extdata/gene_promoters_encode1kb_mm10.rds")
+
+#protein_coding
+#uncomment to make promoters for encode genes
+#usethis::use_data_raw("gene_promoters_encode1kb")
+usethis::use_data(gene_promoters_encode1kb_mm10, overwrite = TRUE)
+usethis::use_data(gene_promoters_encode1kb_mm10_proteinCoding, overwrite = TRUE)
